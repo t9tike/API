4a3fe129db82dc7444b8c736f29783b72484a774
Keijo <keti.91@hotmail.com>
2021-10-04 20:02:13 +0300

    second update

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+var dateTime = require('node-datetime');
+
+app.use(bodyParser.json());
+
+let userDb = [];
+let itemDb = [];
+
+passport.use(new BasicStrategy(
+    (username, password, done) => {
+        console.log('Basic strategy params, username ' + username + " , password " + password);
+
+        //credintial check
+        //search userDb for matchin user and pasworrd
+
+        const searchResult = userDb.find(user => {
+            if(user.username === username){
+                if(bcrypt.compareSync(password, user.password)) {
+                    return true;
+                }
+            }
+        return false;
+        })     
+        //const searchResult = userDb.find(user => ({(username === user.username) && (password === user.password)))
+        
+        if(searchResult != undefined) {
+            done(null, {}); //sucsess
+        }else {
+            done(null, false); //not
+        }
+    }
+));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/protectedResource', passport.authenticate('basic', { session: false}),(req, res) =>{
+    res.send("onnistui jeee")
+
+})
+
+//this route will recive data structure
+app.post('/signup', (req, res) => {
+
+    const salt = bcrypt.genSaltSync(6)
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt)
+
+    const newUser = {
+     username: req.body.username,
+     password: hashedPassword,
+     email: req.body.email,
+ }
+ userDb.push(newUser);
+ res.sendStatus(201);
+})
+
+//uusi posti
+app.post('/items', passport.authenticate('basic', { session: false}),(req, res) => {
+
+    var dt = dateTime.create();
+    var formatted = dt.format('Y-m-d H:M:S');
+    String(formatted);
+
+
+
+    const newItem = {
+        id: uuidv4(),
+        title: req.body.title,
+        // Description: req.body.description,
+        // Category: req.body.category,
+        // Address: req.body.Address,
+        // PostalCode: req.body.postalCode,
+        // Asking_Price: req.body.Asking_Price,
+        Date_of_Posting: formatted,
+        // Delivery_Type: req.body.Delivery_Type,
+        // Sellers_Info: req.body.Sellers_Info,
+    }
+    itemDb.push(newItem);
+    res.sendStatus(201);
+    res.send("uusi itemi luotu")
+})
+
+
+
+
+
+
+app.get('/items', (req, res) => {
+
+    res.json(itemDb);
+})
+
+app.get('/items/:id', (req, res) => {
+    const item = itemDb.find(d => d.id === req.params.id);
+    if (item === undefined) {
+        res.sendStatus(404);
+    } else {
+        res.json(item);
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
